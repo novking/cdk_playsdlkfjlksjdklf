f157340952a3393f06f997269577364f447764ce
Aaron Zhang <awesome1aaron@gmail.com>
2024-05-07 12:00:50 -0700

    save

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import Stack
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_networkfirewall as networkfirewall
-from aws_cdk import Fn, CfnOutput
+from aws_cdk import Fn, CfnOutput, CfnTag
 
 
 class FirewallStack(Stack):
@@ -26,7 +26,21 @@ class FirewallStack(Stack):
                     subnet_type=ec2.SubnetType.PUBLIC,
                 ),
             ],
+            create_internet_gateway=False
         )
+
+                # Create an Internet Gateway
+        igw = ec2.CfnInternetGateway(self, "AaronternetGateway12", tags=[CfnTag(
+                key="name",
+                value="AaronternetGateway12"
+            )])
+
+        # Attach the Internet Gateway to the VPC
+        igw_attachment = ec2.CfnVPCGatewayAttachment(
+            self, "AaronIGWAttach4", vpc_id=vpc.vpc_id, internet_gateway_id=igw.ref
+        )
+
+        
 
         firewall_subnet_selection = vpc.select_subnets(
             subnet_group_name="FirewallSubnet"
@@ -39,6 +53,64 @@ class FirewallStack(Stack):
         workload_subnet_selection_id = Fn.select(
             0, workload_subnet_selection.subnet_ids
         )
+
+        # Create and configure route tables
+        # Route table for the firewall subnet
+        firewall_route_table = ec2.CfnRouteTable(
+            self,
+            "AAFirewallRouteTable1",
+            vpc_id=vpc.vpc_id,
+            tags=[CfnTag(key="Name", value="AAFirewallRouteTable1")],
+        )
+
+        # Default route through the internet gateway for the firewall subnet
+        ec2.CfnRoute(
+            self,
+            "AAFirewallDefaultRoute",
+            route_table_id=firewall_route_table.ref,
+            destination_cidr_block="0.0.0.0/0",
+            gateway_id=igw.ref,
+        )
+
+        # Route table for the workload subnet
+        workload_route_table = ec2.CfnRouteTable(
+            self,
+            "AAWorkloadRouteTable1",
+            vpc_id=vpc.vpc_id,
+            tags=[CfnTag(key="Name", value="AAWorkloadRouteTable1")],
+        )
+
+        # Routes for the workload subnet
+        # ec2.CfnRoute(
+        #     self,
+        #     "AAWorkloadToLocal",
+        #     route_table_id=workload_route_table.ref,
+        #     destination_cidr_block="10.0.0.0/16",
+        #     gateway_id="local",
+        # )
+
+
+
+        # Associate route tables with the subnets
+        ec2.CfnSubnetRouteTableAssociation(
+            self,
+            f"AARTAssocFirewall",
+            subnet_id=firewall_subnet_id,
+            route_table_id=firewall_route_table.ref,
+        )
+
+        ec2.CfnSubnetRouteTableAssociation(
+            self,
+            f"AARTAssocWorkload",
+            subnet_id=workload_subnet_selection_id,
+            route_table_id=workload_route_table.ref,
+        )
+
+
+        CfnOutput(self, "FirewallSubnetId", value=firewall_subnet_id)
+        CfnOutput(self, "WorkloadSubnetId", value=workload_subnet_selection_id)
+        CfnOutput(self, "workload_route_table", value=workload_route_table.attr_route_table_id)
+
 
         rule_string = """
         pass http any any -> any any (http.host; content:"aws.amazon.com"; endswith; msg:"Allow HTTP access to AWS services"; sid:1000001; rev:1;)
@@ -85,71 +157,20 @@ class FirewallStack(Stack):
             vpc_id=vpc.vpc_id,
         )
 
-        # Create an Internet Gateway
-        igw = ec2.CfnInternetGateway(self, "AaronternetGateway12")
 
-        # # Attach the Internet Gateway to the VPC
-        # igw_attachment = ec2.CfnVPCGatewayAttachment(
-        #     self, "AaronIGWAttach4", vpc_id=vpc.vpc_id, internet_gateway_id=igw.ref
-        # )
 
-        # base_internet_gateway = CfnOutput(self, id='IgwId', value=vpc.internet_gateway_id, export_name='my-igw-id')
 
-        # Create and configure route tables
-        # Route table for the firewall subnet
-        firewall_route_table = ec2.CfnRouteTable(
-            self,
-            "FirewallRouteTable",
-            vpc_id=vpc.vpc_id,
-        )
+        CfnOutput(self, "firewall_endpoints", value=Fn.select(0, firewall.attr_endpoint_ids))
 
-        # Default route through the internet gateway for the firewall subnet
-        # ec2.CfnRoute(
-        #     self,
-        #     "FirewallDefaultRoute",
-        #     route_table_id=firewall_route_table.ref,
-        #     destination_cidr_block="0.0.0.0/0",
-        #     gateway_id=igw.ref,
-        # )
-
-        # Route table for the workload subnet
-        workload_route_table = ec2.CfnRouteTable(
-            self,
-            "WorkloadRouteTable",
-            vpc_id=vpc.vpc_id,
-        )
-
-        # Routes for the workload subnet
-        # ec2.CfnRoute(
-        #     self,
-        #     "WorkloadToLocal",
-        #     route_table_id=workload_route_table.ref,
-        #     destination_cidr_block="10.0.0.0/16",
-        #     gateway_id="local",
-        # )
 
         # ec2.CfnRoute(
         #     self,
-        #     "WorkloadToFirewall",
+        #     "AAWorkloadToFirewall",
         #     route_table_id=workload_route_table.ref,
         #     destination_cidr_block="0.0.0.0/0",
         #     gateway_id=Fn.select(0, firewall.attr_endpoint_ids),
         # )
 
-        # Associate route tables with the subnets
-        ec2.CfnSubnetRouteTableAssociation(
-            self,
-            f"RTAssocFirewall",
-            subnet_id=firewall_subnet_id,
-            route_table_id=firewall_route_table.ref,
-        )
-
-        ec2.CfnSubnetRouteTableAssociation(
-            self,
-            f"RTAssocWorkload",
-            subnet_id=workload_subnet_selection_id,
-            route_table_id=workload_route_table.ref,
-        )
 
     # sudo su
     # yum install -y httpd
